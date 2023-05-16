20f90e7467439edc77e03cfe11e8faf10cbd55f7
Tyler E. Harman <50343995+teharman@users.noreply.github.com>
2023-05-16 12:00:14 -0400

    Initial development of script - stackObject segmentation and export of images/csv files

--- sandbox_script.R
+++ sandbox_script.R
@@ -16,10 +16,10 @@ library(DT)
 
 # clear environment and free unused memory
 gc()
-#test
 
-#Change directories here
+####Change directories/Import images####
 img_dir <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/version_2/5_4_demo/NZ_Anabena/40x_AccuScope/")
+image_savdir <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/version_2/5_4_demo/NZ_Anabena/40x_AccuScope")
 images <- list.files(img_dir, pattern = "tif", full.name = T)
 images_names <- list.files(img_dir, pattern = "tif", full.name = F)
 
@@ -28,13 +28,10 @@ read_images <- lapply(images, readTIFF)
 img_transposed <- lapply(read_images,aperm,c(2,1,3))
 names(images) <- imgNames
 
+
+####Initial image conversion####
 grey_imgs<-lapply(img_transposed, greyscale, contrast = 0.2)
 display(grey_imgs[[1]])
-
-#f=array(1,dim=c(3,3))
-#f=f/sum(f)
-#filter_imgs<-filter2(grey_imgs[[1]],f)
-#display(filter_imgs)
 
 binary<-function(x, adj = 0.5) {
   binary_img<- x > adj
@@ -44,7 +41,6 @@ img_neg<-function(x) {
   imgneg<-max(x)-x
   return(imgneg)
 }
-#lp_imgs<-lapply(grey_imgs, lp_filter, size=51,sigma=1)
 neg_imgs<-lapply(grey_imgs, img_neg)
 display(neg_imgs[[1]])
 binary_img<-lapply(neg_imgs, binary, adj = 0.4)
@@ -54,8 +50,8 @@ imagesMapped <- lapply(binary_img, mapped, threshold = 0.2) #background intensit
 
 img_watershed<-single_cell_convert(imagesMapped[[1]],w=50,h=50,offset=0.001,areathresh=250,tolerance=0.8,ext = 3)
 display(img_watershed)
-final_img<-count_images(img_watershed,normalize = T, removeEdgeCells = T)
-display(final_img)
+
+####Shiny UI cell selector####
 
 jscode <- "shinyjs.closeWindow = function() { window.close(); }"
 seed.input <<- data.frame(x_axis = character(0), y_axis = numeric(0))
@@ -71,8 +67,6 @@ create_image <- function(loaded_image, image_data) {
     color = "black")
   return(displayed_image)
 }
-
-
 
 ui <- fixedPage(
   useShinyjs(),
@@ -132,6 +126,7 @@ server <- function(input, output, session) {
 runGadget(ui, server, viewer = dialogViewer("cellcount Image Analysis Interface",
                                             width = 1000, height = 2300))
 
+####Seed segmentation####
 seed.input<-lapply(seed.input,as.numeric)
 seed.input<-as.data.frame(seed.input)
 seed.input<-round(seed.input,0)
@@ -147,7 +142,6 @@ for (j in 2:nrow(seed.input)){
 seed.mtx<-seed.mtx[,c(1024:1),drop = FALSE]
 
 display(seed.mtx)
-display(imagesMapped[[2]])
 
 seed.mtx.img<-Image(seed.mtx)
 display(seed.mtx.img)
@@ -159,8 +153,67 @@ cmask<-propagate(neg_imgs[[1]],seeds=seed_img,mask=ctmask,lambda = 10^1)
 display(cmask)
 segmented<-paintObjects(cmask,grey_imgs[[1]],col = c('pink','red'))
 display(segmented,all=TRUE)
-st <- stackObjects(cmask,img_watershed)
-display(st)
+st_blob <- stackObjects(cmask,img_watershed)
+display(st_blob)
+st_img <- stackObjects(cmask,grey_imgs[[1]])
+display(st_img)
+
+
+####Create features and export images####
+features.data.blob<-computeFeatures(cmask,img_watershed)
+features.data.img<-computeFeatures(cmask,grey_imgs[[1]])
+features.blob<-as.data.frame(features.data.blob)
+features.img<-as.data.frame(features.data.img)
+features.blob1<-cbind(features.blob,frame_num=NA)
+features.img1<-cbind(features.blob,frame_num=NA)
+
+#blob st img save
+Index_blob<-paste0(sub(".tif", replacement = "blob_", x=imgNames[[1]]))
+Index1<-paste0(sub(".tif", replacement = "/ ", x=imgNames[[1]]))
+newpath<-file.path(image_savdir,Index1)
+if(!dir.exists(newpath)){
+  dir.create(newpath)
+}
+for(k in 1:dim(st_blob)[3]) {
+  st_imgs_blob<-st_blob[, , k]
+  analyzed_image1<-paste0(sub(".tif", replacement = " ", x=imgNames[[1]]),"_frame")
+  analyzed_image2<-paste0(sub(".tif", replacement = " ", x=analyzed_image1),k)
+  analyzed_image3<-paste0(sub(".tif", replacement = " ", x=analyzed_image2),"_blob_analyzed.tiff")
+  features.blob1$frame_num[k]<-cbind(k)
+  writeImage(st_imgs_blob,files = paste0(newpath, analyzed_image3),compression=c("LZW"))
+}
+csv_save<-paste0(paste(Index_blob,Sys.Date()),".csv")
+write.csv(features.blob1, paste0(newpath, csv_save)) #Change this CSV file name
+
+#grey st img save
+Index_grey<-paste0(sub(".tif", replacement = "grey_", x=imgNames[[1]]))
+Index1<-paste0(sub(".tif", replacement = "/ ", x=imgNames[[1]]))
+newpath<-file.path(image_savdir,Index1)
+if(!dir.exists(newpath)){
+  dir.create(newpath)
+}
+for(k in 1:dim(st_img)[3]) {
+  st_imgs_grey<-st_img[, , k]
+  analyzed_image1<-paste0(sub(".tif", replacement = " ", x=imgNames[[1]]),"_frame")
+  analyzed_image2<-paste0(sub(".tif", replacement = " ", x=analyzed_image1),k)
+  analyzed_image3<-paste0(sub(".tif", replacement = " ", x=analyzed_image2),"_grey_analyzed.tiff")
+  features.blob1$frame_num[k]<-cbind(k)
+  writeImage(st_imgs_grey,files = paste0(newpath, analyzed_image3),compression=c("LZW"))
+}
+csv_save<-paste0(paste(Index_grey,Sys.Date()),".csv")
+write.csv(features.img1, paste0(newpath, csv_save)) #Change this CSV file name
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+####Misc code####
 
 #max(bwlabel(img_watershed))
 #table(bwlabel(img_watershed))
