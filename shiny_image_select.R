ee5cd53ff237759182dc20aa0d03798d64b1b247
Tyler E. Harman <50343995+teharman@users.noreply.github.com>
2023-05-17 14:21:37 -0400

    Updated development - sucessfully visualized stackObject images

--- shiny_image_select.R
+++ shiny_image_select.R
@@ -1,36 +1,87 @@
+library(EBImage)
+library(tiff)
+library(png)
+library(pixmap)
+library(raster)
+library(cellcount)
+library(dplyr)
+library(jjb)
 library(shiny)
+library(magick)
+library(ggplot2)
+library(shinyalert)
+library(shinyFiles)
+library(shinyjs)
+library(DT)
 
-ui_test <- fluidPage(
+jscode <- "shinyjs.closeWindow = function() { window.close(); }"
 
-  titlePanel("Slideshow"),
+image_num1<<-textConnection('image_num2','wr',local=FALSE)
 
+ui2 <- fluidPage(
+  useShinyjs(),
+  extendShinyjs(text = jscode, functions = c("closeWindow")),
   sidebarLayout(
     sidebarPanel(
-      actionButton("previous", "Previous"),
-      actionButton("next", "Next")
+      actionButton("previous", "Previous", icon=icon("arrow-left")),
+      actionButton("next", "Next", icon=icon("arrow-right")),
+      h6(" "),
+      h6(" "),
+      textOutput("image_num"),
+      h6(" "),
+      h6(" "),
+      actionButton("run1", "Select Image Removal",class = "btn-success",icon=icon("trash")),
+      h6(" "),
+      h6(" "),
+      actionButton("close", "Close window",class = "btn-danger",icon=icon("check"))
     ),
-
     mainPanel(
-      imageOutput("image")
+      plotOutput("current_image_plot")
     )
   )
 )
 
-server_test <- function(input, output, session) {
-
+server2 <- function(input, output, session) {
   index <- reactiveVal(1)
 
   observeEvent(input[["previous"]], {
     index(max(index()-1, 1))
   })
+
   observeEvent(input[["next"]], {
-    index(min(index()+1, length(st_blob_test)))
+    index(min(index()+1, dim(st_img_test)[3]))
   })
 
-  output$image <- renderDisplay({
-    display(st_blob_test)
+  output$current_image_plot <- renderPlot({
+    loaded_image <- magick::image_ggplot(image_read(st_img_test[,,index()]))
+    loaded_image
+  },res=300,width=350,height=350)
+
+  output$image_num<-renderText({
+    paste('Image number: ',index())
   })
+
+  observeEvent(input$run1, {
+    shinyalert::shinyalert('Enter image numbers for removal',
+                           type='input',callbackR=image_number, showCancelButton = TRUE,
+                           inputPlaceholder = 'Example: 1, 2, 3, 4, 5',
+                           size="m")
+  })
+
+  image_number<-function(value4) {
+    sink(image_num1)
+    save_name<-cat(value4)
+    sink()
+    close(image_num1)
+    image_num2<<-scan(text=image_num2,dec=",")
+  }
+
+  observeEvent(input$close, {
+    js$closeWindow()
+    shinyjs::stopApp()
+  })
+
 }
 
-# Run the application
-shinyApp(ui = ui_test, server = server_test)
+runGadget(ui2, server2, viewer = dialogViewer("Cell Image Selector",
+                                            width = 800, height = 400))
