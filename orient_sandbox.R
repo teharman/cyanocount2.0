7d69d16aaa14ada3ac198baacadafba4545f350b
Tyler E. Harman <50343995+teharman@users.noreply.github.com>
2023-07-20 16:05:11 -0400

    New script to generate varying orientations of input images - helps boost overall image library numbers for models

---
+++ orient_sandbox.R
@@ -0,0 +1,53 @@
+library(EBImage)
+library(tidyverse)
+library(tiff)
+library(png)
+library(pixmap)
+library(raster)
+library(cellcount)
+library(dplyr)
+library(jjb)
+library(shiny)
+library(magick)
+library(ggplot2)
+library(shinyalert)
+library(shinyFiles)
+library(shinyjs)
+library(DT)
+library(cyanocount2.0)
+
+####START ANALYSIS####
+# clear environment and free unused memory
+gc()
+
+#Change directories/Import images
+img_dir <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/CyanoSCOPE_imgs/AccuScope/Microcystis_F192/40X/Raw_Imgs/Batch_7")
+orient_savdir1 <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/CyanoSCOPE_imgs/AccuScope/Microcystis_F192/40X/Raw_Imgs/Batch_26/")
+orient_savdir2 <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/CyanoSCOPE_imgs/AccuScope/Microcystis_F192/40X/Raw_Imgs/Batch_27/")
+orient_savdir3 <- ("C:/Users/Tyler.Harman/Desktop/cellcount_work/CyanoSCOPE_imgs/AccuScope/Microcystis_F192/40X/Raw_Imgs/Batch_28/")
+images <- list.files(img_dir, pattern = "tif", full.name = T)
+images_names <- list.files(img_dir, pattern = "tif", full.name = F)
+
+imgNames <- paste0(images_names)
+read_images <- lapply(images, image_read)
+names(images) <- imgNames
+
+for (j in 1:length(read_images)){
+  img_flip<-image_flip(read_images[[j]])
+  analyzed_image1<-paste0(sub(".tif", replacement = " ", x=imgNames[[j]]),"_2.tif")
+  image_write(img_flip,path = paste0(orient_savdir1, analyzed_image1))
+}
+
+for (j in 1:length(read_images)){
+  img_flop<-image_flop(read_images[[j]])
+  analyzed_image1<-paste0(sub(".tif", replacement = " ", x=imgNames[[j]]),"_3.tif")
+  image_write(img_flop,path = paste0(orient_savdir2, analyzed_image1))
+}
+
+for (j in 1:length(read_images)){
+  img_flop<-image_flop(read_images[[j]])
+  img_flip<-image_flip(img_flop)
+  analyzed_image1<-paste0(sub(".tif", replacement = " ", x=imgNames[[j]]),"_4.tif")
+  image_write(img_flip,path = paste0(orient_savdir3, analyzed_image1))
+}
+
