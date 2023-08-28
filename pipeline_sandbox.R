16fac31c172017e9e08e208cc99490a3e3c8f4ce
Tyler E. Harman <50343995+teharman@users.noreply.github.com>
2023-08-28 16:37:24 -0400

    Updates to UI - new prediction outputs and UI visualization

--- pipeline_sandbox.R
+++ pipeline_sandbox.R
@@ -26,6 +26,7 @@ library(shinyBS)
 library(DT)
 library(shinythemes)
 library(cyanocount2.0)
+library(RSAGA)
 #___________________________________________________________#
 
 jscode <- "shinyjs.closeWindow = function() { window.close(); }"
@@ -51,12 +52,12 @@ ui_main = fluidPage(
   "Developed by NOAA NCCOS HAB-F and UCSD Qualcomm Institute"),
   mainPanel(navbarPage(
     title=h4("CyanoSCOPE Tools"),
-    tabPanel(h5("1) Image(s) Selection and Upload"),
+    tabPanel(h6("1) Image Selection and Upload"),
              fluidRow(
-               shinyDirButton('path1','1) Select Images','Please select a folder containing images',FALSE,class = "btn-success",style='height:35px;width:175px;font-size:100%;display:center-align',icon=icon("folder-open")),
-               actionButton("update1","2) Update Data",class = "btn-success",style='height:35px;width:175px;font-size:100%;display:center-align',icon=icon("arrows-rotate")),
-               actionButton("run1","3) Upload Images",class = "btn-success",style='height:35px;width:175px;font-size:100%;display:center-align',icon=icon("upload")),
-               actionButton("update2","4) View Images",class = "btn-success",style='height:35px;width:175px;font-size:100%;display:center-align',icon=icon("camera"))
+               shinyDirButton('path1','1) Select Images','Please select a folder containing images',FALSE,class = "btn-success",style='height:35px;width:185px;font-size:100%;display:center-align',icon=icon("folder-open")),
+               actionButton("update1","2) Update Data",class = "btn-success",style='height:35px;width:185px;font-size:100%;display:center-align',icon=icon("arrows-rotate")),
+               actionButton("run1","3) Upload Images",class = "btn-success",style='height:35px;width:185px;font-size:100%;display:center-align',icon=icon("upload")),
+               actionButton("update2","4) View Images",class = "btn-success",style='height:35px;width:185px;font-size:100%;display:center-align',icon=icon("camera"))
              ),
              h6(" "),
              fluidRow(
@@ -64,7 +65,7 @@ ui_main = fluidPage(
              ),
              fluidRow(
                column(5,
-                      wellPanel(DT::dataTableOutput('img_data'),width=3,style = "font-size:70%")
+                      wellPanel(DT::dataTableOutput('img_data'),width=3,style = "font-size:70%;height:400px")
                ),
                column(7,
                       wellPanel(plotOutput("current_image_plot"),style = "padding: 0px;")
@@ -77,7 +78,7 @@ ui_main = fluidPage(
                       actionButton("next", "Next", icon=icon("arrow-right"),style='height:35px;width:150px;font-size:120%;display:center-align'))
              )
     ),
-    tabPanel(h5("2) Binary Segmentation Modeling"),
+    tabPanel(h6("2) Binary Segmentation Modeling"),
              fluidRow(
                actionButton("load1","1) Load Segmentation Model",class = 'btn-success',style='height:35px;width:225px;font-size:100%;display:center-align',icon=icon("upload")),
                actionButton("run2","2) Run Binary Segmentation",class = 'btn-success',style='height:35px;width:225px;font-size:100%;display:center-align',icon=icon('person-running'))
@@ -91,41 +92,58 @@ ui_main = fluidPage(
                actionButton("next1", "Next", icon=icon("arrow-right"),style='height:35px;width:150px;font-size:120%;display:center-align')
              )
     ),
-    tabPanel(h5("3) ID Prediction Modeling"),
+    tabPanel(h6("3) ID Prediction Modeling"),
              fluidRow(
-               actionButton("run3","1) Run ID Prediction",class = 'btn-success',style='height:35px;width:225px;font-size:100%;display:center-align',icon=icon('magnifying-glass')),
-               actionButton("update3","2) Update Predict Data",class = "btn-success",style='height:35px;width:225px;font-size:100%;display:center-align',icon=icon("arrows-rotate")),
-               actionButton("update4","3) View Segmented Cells",class = "btn-success",style='height:35px;width:225px;font-size:100%;display:center-align',icon=icon("camera"))
+               actionButton("run3","1) Apply Segmentation",class = 'btn-success',style='height:35px;width:185px;font-size:90%;display:center-align',icon=icon('magnifying-glass')),
+               actionButton("update4","2) View Segmented Cells",class = "btn-success",style='height:35px;width:185px;font-size:90%;display:center-align',icon=icon("camera")),
+               actionButton("update3","3) Run ID Prediction",class = "btn-success",style='height:35px;width:185px;font-size:100%;display:center-align',icon=icon("person-running")),
+               actionButton("update5","4) Update Data",class = "btn-success",style='height:35px;width:185px;font-size:100%;display:center-align',icon=icon("arrows-rotate"))
              ),
              h6(" "),
              fluidRow(
-               column(5,
-                      wellPanel(DT::dataTableOutput('predict_data'),width=3,style = "font-size:70%")
+               column(9,
+                      wellPanel(DT::dataTableOutput('predict_data',width = '500px'),style = "font-size:70%;height:450px")
                ),
-               column(7,
-                      wellPanel(plotOutput("current_image_plot2"),style = "padding: 0px;")
+               column(3,
+                      wellPanel(plotOutput("current_image_plot2"),style = "padding: 0px;height:150px")
                )
              ),
+             h4(" "),
+             h4(" "),
              fluidRow(
-               column(5,
+               column(8,
                       textOutput("image_info"),placeholder=TRUE,style="font-size:105%;font-weight:bold"),
-               column(4,
-                      actionButton("previous2", "IMG_Previous", icon=icon("arrow-left"),style='height:35px;width:150px;font-size:100%;display:center-align')),
-               column(3,
-                      actionButton("next2", "IMG_Next", icon=icon("arrow-right"),style='height:35px;width:150px;font-size:100%;display:center-align'))
+               column(2,
+                      actionButton("previous2", "IMG_Previous", icon=icon("arrow-left"),style='height:35px;width:125px;font-size:90%;display:center-align')),
+               column(2,
+                     actionButton("next2", "IMG_Next", icon=icon("arrow-right"),style='height:35px;width:125px;font-size:100%;display:center-align'))
              ),
              fluidRow(
                column(2,
                       textOutput("image_num1"),placeholder=TRUE,style="font-size:105%;font-weight:bold"),
-               column(3,
+               column(6,
                       textOutput("seg_num"),placeholder=TRUE,style="font-size:105%;font-weight:bold"),
-               column(4,
-                      actionButton("previous3", "Cell_Previous", icon=icon("arrow-left"),style='height:35px;width:150px;font-size:100%;display:center-align')),
-               column(3,
-                      actionButton("next3", "Cell_Next", icon=icon("arrow-right"),style='height:35px;width:150px;font-size:100%;display:center-align'))
+               column(2,
+                      actionButton("previous3", "Cell_Previous", icon=icon("arrow-left"),style='height:35px;width:125px;font-size:90%;display:center-align')),
+               column(2,
+                      actionButton("next3", "Cell_Next", icon=icon("arrow-right"),style='height:35px;width:125px;font-size:100%;display:center-align'))
              )
     ),
-    tabPanel(h5("4) CyanoSCOPE analysis")
+    tabPanel(h6("4) CyanoSCOPE analysis"),
+             fluidRow(
+               actionButton("run4","1) Generate Predict Image",class = 'btn-success',style='height:35px;width:225px;font-size:100%;display:center-align',icon=icon('wrench')),
+               actionButton("save1","2) Save Images/Datasets",class = 'btn-success',style='height:35px;width:225px;font-size:100%;display:center-align',icon=icon('save'))
+             ),
+             h6(" "),
+             column(12,
+                    wellPanel(plotOutput("current_image_plot3"),style = "padding: 0px;height:500px")
+             ),
+             fluidRow(
+               column(7,offset=1,
+                      actionButton("previous4", "Previous", icon=icon("arrow-left"),style='height:35px;width:125px;font-size:100%;display:center-align')),
+               column(3,offset=1,
+                      actionButton("next4", "Next", icon=icon("arrow-right"),style='height:35px;width:125px;font-size:100%;display:center-align'))
+             )
     ),
     tags$style(type = 'text/css', '.navbar { background-color: #303030;
                            font-family: Arial;
@@ -185,7 +203,7 @@ server_main = function(input, output, session) {
     output$current_image_plot <- renderPlot({
       loaded_image <- magick::image_ggplot(image_read(read_images[[index()]]))
       loaded_image
-    },res=300,width=375,height=375)
+    },res=300,width=400,height=400)
   })
 
   ####server - tab 2####
@@ -225,6 +243,7 @@ server_main = function(input, output, session) {
   })
 
   ####server - tab 3####
+  ID.input <<- data.frame(file_ID = character(0), cell_number = numeric(0), ID_estimate = character(0), Percent_estimate = numeric(0))
   index2 <- reactiveVal(1)
   index3 <- reactiveVal(1)
   observeEvent(input[["previous2"]], {
@@ -249,8 +268,8 @@ server_main = function(input, output, session) {
     paste('Cell #: ',index3())
   })
   observeEvent(input$run3,{
-    message("running ID prediction model - please wait")
-    predict_model<<-load_model_tf('C:/Users/Tyler.Harman/Desktop/cellcount_work/CyanoSCOPE_imgs/AccuScope/Draft_Model/small_data/model/', custom_objects = NULL, compile = TRUE)
+    message("applying binary segmentation - please wait")
+    predict_model<<-load_model_tf('C:/Users/Tyler.Harman/Desktop/cellcount_work/CyanoSCOPE_imgs/AccuScope/Draft_Model/models/ID_predict_model/ID_model_test/', custom_objects = NULL, compile = TRUE)
     watershed_convert <- function(x, w = 17, h = 17, offset = 0.001, areathresh = 50, tolerance= 0.5, ext = 1, removeEdgeCells = TRUE) {
       if (removeEdgeCells == TRUE){
         image <- thresh(x, w = w, h = h, offset = offset)
@@ -274,66 +293,95 @@ server_main = function(input, output, session) {
         return(image3)
       }
     }
-    test_img<-image_load(images[[6]],target_size = c(1024,1024))
+    test_img<-image_load(images[[1]],target_size = c(1024,1024))
     img_array<-test_img%>%image_to_array()%>%'/'(255)
     rgb.imgs<-Image(img_array,colormode = Color)
-    mask<-abind(mask_main[[6]])
+    mask<-abind(mask_main[[1]])
     mask<-mask[,,1]
-    display(mask)
     img_watershed<-watershed_convert(mask,w=50,h=50,offset=0.001,areathresh=50,tolerance=0.5,ext = 1,removeEdgeCells=TRUE)
     ctmask<-opening(img_watershed>0.1,makeBrush(5,shape='disc'))
     seed_mask<-single_cell_convert(ctmask)
     cmask<-propagate(mask,seeds=seed_mask,mask=ctmask,lambda = 10^1)
     cmask1<-array_reshape(cmask,c(dim(cmask),1))
-    display(rgb.imgs)
-    display(cmask1)
     segmented<-paintObjects(cmask,rgb.imgs,col = c('black','orange'))
-    display(segmented,all=TRUE)
     st_img <- stackObjects(cmask,rgb.imgs)
     st_img_test <- Image(st_img)
     cell_seg <<- list(st_img_test)
-    for (z in 7:length(read_images)){
+    for (z in 2:length(read_images)){
       test_img<-image_load(images[[z]],target_size = c(1024,1024))
       img_array<-test_img%>%image_to_array()%>%'/'(255)
       rgb.imgs<-Image(img_array,colormode = Color)
       mask<-abind(mask_main[[z]])
       mask<-mask[,,1]
-      display(mask)
       img_watershed<-watershed_convert(mask,w=50,h=50,offset=0.001,areathresh=5,tolerance=0.5,ext = 1,removeEdgeCells=TRUE)
       ctmask<-opening(img_watershed>0.1,makeBrush(5,shape='disc'))
       seed_mask<-single_cell_convert(ctmask)
       cmask<-propagate(mask,seeds=seed_mask,mask=ctmask,lambda = 10^1)
       cmask1<-array_reshape(cmask,c(dim(cmask),1))
-      display(rgb.imgs)
-      display(cmask1)
       segmented<-paintObjects(cmask,rgb.imgs,col = c('black','orange'))
-      display(segmented,all=TRUE)
       st_img <- stackObjects(cmask,rgb.imgs)
       seg_cell<-list(st_img)
       seg_cell_test<-Image(seg_cell)
       cell_seg<<-append(cell_seg,seg_cell_test)
     }
     beepr::beep(sound=1)
-    message("cyanobacteria ID prediction complete")
+    message("***segmentation applied***")
   })
   observeEvent(input$update3,{
-    model_label<-dir("C:/Users/Tyler.Harman/Desktop/cellcount_work/CyanoSCOPE_imgs/AccuScope/Draft_Model/misc/Train/")
-    resize_img<-resize(img_select[,,,1],w=100,h=100)
-    x <- image_to_array(resize_img)
-    x <- array_reshape(x, c(1, dim(x)))
-    x <- x/255
-    pred <- predict_model %>% predict(x)
-    pred <- data.frame("Species" = model_label, "Probability" = t(pred))
-    pred <- pred[order(pred$Probability, decreasing=T),][1:2,]
-    pred$Probability <- paste(format(100*pred$Probability,2),"%")
-    pred_list<<-read.table(text=image_names,col.names = c('img_name'))
+    message("loading ID prediction model - please wait")
+    for(k in 1:length(cell_seg)) {
+      cell_img<-cell_seg[[k]]
+      file_ID <- image_names[[k]]
+      for(z in 1:dim(cell_img)[4]){
+        cell_num<-z
+        resize_img<-resize(cell_img[,,,z],w=150,h=150)
+        x <- image_to_array(resize_img)
+        x <- array_reshape(x, c(1, dim(x)))
+        x <- x/255
+        pred <- predict_model %>% predict(x)
+        pred <- data.frame("Species" = c("Anabaena","Microcystis"), "Probability" = t(pred))
+        pred <- pred[order(pred$Probability, decreasing=T),][1:2,]
+        pred$Probability <- paste(format(100*pred$Probability,2,nsmall=2),"%")
+        ID.input[nrow(ID.input) + 1, ] <<- c(file_ID, cell_num, pred$Species[[1]], pred$Probability[[1]])
+      }
+    }
+    message("***ID prediction complete***")
+    beepr::beep(sound=1)
+  })
+  rv1 <- reactiveVal(ID.input)
+  onclick("update5",{
+    proxy=dataTableProxy("predict_data")
+    replaceData(proxy,ID.input)
+  })
+  output$predict_data <- DT::renderDataTable ({
+    DT::datatable(rv1(), editable = TRUE)
   })
   observeEvent(input$update4,{
     output$current_image_plot2 <- renderPlot({
       img_select<<-(cell_seg[[index2()]])
       loaded_image2 <- magick::image_ggplot(image_read(img_select[,,,index3()]))
       loaded_image2
-    },res=300,width=375,height=375)
+    },res=300,width=150,height=150)
+  })
+
+  ####server - tab 4####
+  observeEvent(input$run4,{
+    message("generating prediction image - please wait")
+    coord.mtx<-RSAGA::grid.to.xyz(cmask)
+    filter<-filter(coord.mtx,z>0)
+    cell.coord<-data.frame(xmin = numeric(0), xmax = numeric(0), ymin = numeric(0), ymax = numeric(0))
+    for(k in 1:length(cell_seg)) {
+      cell_img<-cell_seg[[k]]
+      for(j in 1:dim(cell_img)[4]){
+        filter.mtx<-subset(filter,z==j)
+        xmin<-min(filter.mtx$x)
+        xmax<-max(filter.mtx$x)
+        ymin<-min(filter.mtx$y)
+        ymax<-max(filter.mtx$y)
+        cell.coord[nrow(cell.coord) + 1, ] <- c(xmin, xmax, ymin, ymax)
+      }
+    }
+    ID.input<-cbind(ID.input,cell.coord)
   })
 
   ####other####
