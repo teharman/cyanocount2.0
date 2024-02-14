1350da8025fb2889b1b60c80ce8321e505d21953
Tyler E. Harman <50343995+teharman@users.noreply.github.com>
2024-02-14 10:54:08 -0500

    Updates to function - including zoom feature for easy cell selection

--- R/point_select.R
+++ R/point_select.R
@@ -3,8 +3,18 @@
 #' text goes here.
 #'
 
-point_select<-function(){
+point_select <- function(){
   jscode <- "shinyjs.closeWindow = function() { window.close(); }"
+  js <- '$(document).ready(function(){
+          var element = document.getElementById("current_image_plot");
+          var instance = panzoom(element);
+          $("#zoomout").on("dblclick", function(){
+            return false;
+          });
+          $("#zoomin").on("dblclick", function(){
+            return false;
+          });
+        });'
   seed.input <<- data.frame(x_axis = numeric(0), y_axis = numeric(0))
 
   create_image <- function(loaded_image, image_data) {
@@ -22,6 +32,8 @@ point_select<-function(){
     return(displayed_image)
   }
 
+
+
   myImgResource<-('C:/Users/Tyler.Harman/Desktop/cellcount_work/CyanoSCOPE_Logo.png')
 
   ui1 <- fluidPage(
@@ -35,9 +47,18 @@ point_select<-function(){
         actionButton("BRefresh","Refresh",class = "btn-success",style='height:50px;width:285px;font-size:140%',icon=icon("arrows-rotate"),style="display:center-align"),
         h3(" "),
         DT::dataTableOutput('data'),
+        tags$head(
+          tags$script(src = "https://unpkg.com/panzoom@9.4.0/dist/panzoom.min.js"),
+          tags$script(HTML(js))
+        ),
+        actionGroupButtons(
+          inputIds = c("zoomout", "zoomin"),
+          labels = list(icon("minus"), icon("plus")),
+          status = "primary"
+        ),
         width=3,style = "font-size:75%"),
       mainPanel(
-        plotOutput("current_image_plot", dblclick = "double_click", hover = "hover", width = "100%"),
+        plotOutput("current_image_plot", click = "double_click", hover = "hover", width = "100%"),
         h3(" "),
         h3(" ")
       )
@@ -57,7 +78,8 @@ point_select<-function(){
       return(loaded_logo)
     }, height=100,width=285)
 
-    output$current_image_plot <- renderPlot({
+
+    output$current_image_plot <- renderPlot(execOnResize = FALSE,{
       displayed_image <- create_image(loaded_image,
                                       image_data$double_click
       )
@@ -73,11 +95,8 @@ point_select<-function(){
     observeEvent({input$double_click}, {
       clickrow <<- data.frame(x_values = input$double_click$x,
                               y_values = input$double_click$y)
-
       seed.input[nrow(seed.input) + 1, ] <<- c(clickrow$x_values,clickrow$y_values)
-
       image_data$double_click[1,] <<- clickrow
-
     })
 
     onclick("BRefresh",{
