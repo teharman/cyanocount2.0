ccebb3932396829fdbbf8909b1b0591a49c12356
Tyler E. Harman <50343995+teharman@users.noreply.github.com>
2024-10-08 12:24:04 -0400

    Updated script to house features data of new cultures added to CNN models.

--- features_data.R
+++ features_data.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 ####Dolichospermum_F271 20X Data####
 
-Doli_F271 <- ('X:/CyanoSCOPE_imgs/AccuScope/Dolichospermum_F271/20X/Raw_Imgs/')
+Doli_F271 <- ('D:/CyanoSCOPE_imgs/AccuScope/Dolichospermum_F271/20X/Raw_Imgs/')
 
 Doli_F271_BatchFolders <- list.files(Doli_F271)
 
@@ -69,74 +69,9 @@ Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Dolichospermum','F271','20X',
 
 rm(list = setdiff(ls(),c("Main_Shape_Data")))
 
-####Dolichospermum_F271 40X Data####
-
-Doli_F271 <- ('X:/CyanoSCOPE_imgs/AccuScope/Dolichospermum_F271/40X/Raw_Imgs/')
-
-Doli_F271_BatchFolders <- list.files(Doli_F271)
-
-main_data <- list()
-for(i in 1:length(Doli_F271_BatchFolders)){
-  csv_batch <- list()
-  Batch_Folder <- Doli_F271_BatchFolders[[i]]
-  Batch_Folder_path <- paste0(Doli_F271,Batch_Folder,"/")
-  Image_files <- list.files(path=Batch_Folder_path)
-  if(grepl("(?i).jpg", Image_files[[2]])==TRUE){
-    suppressWarnings(Image_files <- grep(list.files(path=Batch_Folder_path), pattern=c(".jpg"), invert=TRUE, value=TRUE))
-  } else if (grepl("(?i).tif", Image_files[[2]])==TRUE){
-    suppressWarnings(Image_files <- grep(list.files(path=Batch_Folder_path), pattern=c(".tif"), invert=TRUE, value=TRUE))
-  } else if (grepl("(?i).tiff", Image_files[[2]])==TRUE){
-    suppressWarnings(Image_files <- grep(list.files(path=Batch_Folder_path), pattern=c(".tiff"), invert=TRUE, value=TRUE))
-  }
-  for(h in 1:length(Image_files)){
-    Image_file_folder <- Image_files[[h]]
-    Image_folder_path <- paste0(Batch_Folder_path,Image_file_folder,"/")
-    Image_csv_data <- list.files(Image_folder_path,pattern=".csv")
-    main_csv_path <- paste0(Image_folder_path,Image_csv_data[[1]])
-    MAIN_CSV_DATA <- read.csv(main_csv_path,header=T)
-    MAIN_CSV_DATA <- MAIN_CSV_DATA %>% dplyr::select(.,x.0.s.area,
-                                                     x.0.s.perimeter,
-                                                     x.0.s.radius.mean)
-    csv_image_list <- list(MAIN_CSV_DATA)
-    csv_batch <- append(csv_batch,csv_image_list)
-    rm(csv_image_list)
-  }
-  csv_batch <- do.call('rbind',csv_batch)
-  csv_batch_list <- list(csv_batch)
-  main_data <- append(main_data,csv_batch_list)
-  rm(csv_batch_list)
-}
-
-main_data <- do.call('rbind',main_data)
-
-#Area
-cell_area_avg <- mean(main_data$x.0.s.area,na.rm=TRUE)
-cell_area_std <- as.numeric(sd(main_data$x.0.s.area,na.rm=TRUE))
-cell_area_1QR <- (cell_area_avg)-(2*cell_area_std)
-cell_area_3QR <- (cell_area_avg)+(2*cell_area_std)
-
-#Perimeter
-cell_peri_avg <- mean(main_data$x.0.s.perimeter,na.rm=TRUE)
-cell_peri_std <- as.numeric(sd(main_data$x.0.s.perimeter,na.rm=TRUE))
-cell_peri_1QR <- (cell_peri_avg)-(2*cell_peri_std)
-cell_peri_3QR <- (cell_peri_avg)+(2*cell_peri_std)
-
-#Radius
-cell_radius_avg <- mean(main_data$x.0.s.radius.mean,na.rm=TRUE)
-cell_radius_std <- as.numeric(sd(main_data$x.0.s.radius.mean,na.rm=TRUE))
-cell_radius_1QR <- (cell_radius_avg)-(2*cell_radius_std)
-cell_radius_3QR <- (cell_radius_avg)+(2*cell_radius_std)
-
-#Compile 40X Data
-Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Dolichospermum','F271','40X','Area',cell_area_avg,cell_area_std,cell_area_1QR,cell_area_3QR)
-Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Dolichospermum','F271','40X','Perimeter',cell_peri_avg,cell_peri_std,cell_peri_1QR,cell_peri_3QR)
-Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Dolichospermum','F271','40X','Radius',cell_radius_avg,cell_radius_std,cell_radius_1QR,cell_radius_3QR)
-
-rm(list = setdiff(ls(),c("Main_Shape_Data")))
-
 ####Microcystis_F192 20X Data####
 
-Micro_F192 <- ('X:/CyanoSCOPE_imgs/AccuScope/Microcystis_F192/20X/Raw_Imgs/')
+Micro_F192 <- ('D:/CyanoSCOPE_imgs/AccuScope/Microcystis_F192/20X/Raw_Imgs/')
 
 Micro_F192_BatchFolders <- list.files(Micro_F192)
 
@@ -199,17 +134,22 @@ Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Microcystis','F192','20X','Ra
 
 rm(list = setdiff(ls(),c("Main_Shape_Data")))
 
-####Microcystis_F192 40X Data####
+####Microcystis_F108 20X Data####
 
-Micro_F192 <- ('X:/CyanoSCOPE_imgs/AccuScope/Microcystis_F192/40X/Raw_Imgs/')
+Micro_F108 <- ('D:/CyanoSCOPE_imgs/AccuScope/Microcystis_F108/20X/Raw_Imgs/')
 
-Micro_F192_BatchFolders <- list.files(Micro_F192)
+Micro_F108_BatchFolders <- list.files(Micro_F108)
 
 main_data <- list()
-for(i in 1:length(Micro_F192_BatchFolders)){
+for(i in 1:length(Micro_F108_BatchFolders)){
+  if(i==6) next
+  if(i==7) next
+  if(i==8) next
+  if(i==9) next
+  if(i==10) next
   csv_batch <- list()
-  Batch_Folder <- Micro_F192_BatchFolders[[i]]
-  Batch_Folder_path <- paste0(Micro_F192,Batch_Folder,"/")
+  Batch_Folder <- Micro_F108_BatchFolders[[i]]
+  Batch_Folder_path <- paste0(Micro_F108,Batch_Folder,"/")
   Image_files <- list.files(path=Batch_Folder_path)
   if(grepl("(?i).jpg", Image_files[[2]])==TRUE){
     suppressWarnings(Image_files <- grep(list.files(path=Batch_Folder_path), pattern=c(".jpg"), invert=TRUE, value=TRUE))
@@ -257,10 +197,75 @@ cell_radius_std <- as.numeric(sd(main_data$x.0.s.radius.mean,na.rm=TRUE))
 cell_radius_1QR <- (cell_radius_avg)-(2*cell_radius_std)
 cell_radius_3QR <- (cell_radius_avg)+(2*cell_radius_std)
 
-#Compile 40X Data
-Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Microcystis','F192','40X','Area',cell_area_avg,cell_area_std,cell_area_1QR,cell_area_3QR)
-Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Microcystis','F192','40X','Perimeter',cell_peri_avg,cell_peri_std,cell_peri_1QR,cell_peri_3QR)
-Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Microcystis','F192','40X','Radius',cell_radius_avg,cell_radius_std,cell_radius_1QR,cell_radius_3QR)
+#Compile 20X Data
+Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Microcystis','F108','20X','Area',cell_area_avg,cell_area_std,cell_area_1QR,cell_area_3QR)
+Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Microcystis','F108','20X','Perimeter',cell_peri_avg,cell_peri_std,cell_peri_1QR,cell_peri_3QR)
+Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Microcystis','F108','20X','Radius',cell_radius_avg,cell_radius_std,cell_radius_1QR,cell_radius_3QR)
+
+rm(list = setdiff(ls(),c("Main_Shape_Data")))
+
+####Dolichospermum_F199 20X Data####
+
+Doli_F199 <- ('D:/CyanoSCOPE_imgs/AccuScope/Dolichospermum_F199/20X/Raw_Imgs/')
+
+Doli_F199_BatchFolders <- list.files(Doli_F199)
+
+main_data <- list()
+for(i in 1:length(Doli_F199_BatchFolders)){
+  csv_batch <- list()
+  Batch_Folder <- Doli_F199_BatchFolders[[i]]
+  Batch_Folder_path <- paste0(Doli_F199,Batch_Folder,"/")
+  Image_files <- list.files(path=Batch_Folder_path)
+  if(grepl("(?i).jpg", Image_files[[2]])==TRUE){
+    suppressWarnings(Image_files <- grep(list.files(path=Batch_Folder_path), pattern=c(".jpg"), invert=TRUE, value=TRUE))
+  } else if (grepl("(?i).tif", Image_files[[2]])==TRUE){
+    suppressWarnings(Image_files <- grep(list.files(path=Batch_Folder_path), pattern=c(".tif"), invert=TRUE, value=TRUE))
+  } else if (grepl("(?i).tiff", Image_files[[2]])==TRUE){
+    suppressWarnings(Image_files <- grep(list.files(path=Batch_Folder_path), pattern=c(".tiff"), invert=TRUE, value=TRUE))
+  }
+  for(h in 1:length(Image_files)){
+    Image_file_folder <- Image_files[[h]]
+    Image_folder_path <- paste0(Batch_Folder_path,Image_file_folder,"/")
+    Image_csv_data <- list.files(Image_folder_path,pattern=".csv")
+    main_csv_path <- paste0(Image_folder_path,Image_csv_data[[1]])
+    MAIN_CSV_DATA <- read.csv(main_csv_path,header=T)
+    MAIN_CSV_DATA <- MAIN_CSV_DATA %>% dplyr::select(.,x.0.s.area,
+                                                     x.0.s.perimeter,
+                                                     x.0.s.radius.mean)
+    csv_image_list <- list(MAIN_CSV_DATA)
+    csv_batch <- append(csv_batch,csv_image_list)
+    rm(csv_image_list)
+  }
+  csv_batch <- do.call('rbind',csv_batch)
+  csv_batch_list <- list(csv_batch)
+  main_data <- append(main_data,csv_batch_list)
+  rm(csv_batch_list)
+}
+
+main_data <- do.call('rbind',main_data)
+
+#Area
+cell_area_avg <- mean(main_data$x.0.s.area,na.rm=TRUE)
+cell_area_std <- as.numeric(sd(main_data$x.0.s.area,na.rm=TRUE))
+cell_area_1QR <- (cell_area_avg)-(2*cell_area_std)
+cell_area_3QR <- (cell_area_avg)+(2*cell_area_std)
+
+#Perimeter
+cell_peri_avg <- mean(main_data$x.0.s.perimeter,na.rm=TRUE)
+cell_peri_std <- as.numeric(sd(main_data$x.0.s.perimeter,na.rm=TRUE))
+cell_peri_1QR <- (cell_peri_avg)-(2*cell_peri_std)
+cell_peri_3QR <- (cell_peri_avg)+(2*cell_peri_std)
+
+#Radius
+cell_radius_avg <- mean(main_data$x.0.s.radius.mean,na.rm=TRUE)
+cell_radius_std <- as.numeric(sd(main_data$x.0.s.radius.mean,na.rm=TRUE))
+cell_radius_1QR <- (cell_radius_avg)-(2*cell_radius_std)
+cell_radius_3QR <- (cell_radius_avg)+(2*cell_radius_std)
+
+#Compile 20X Data
+Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Dolichospermum','F199','20X','Area',cell_area_avg,cell_area_std,cell_area_1QR,cell_area_3QR)
+Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Dolichospermum','F199','20X','Perimeter',cell_peri_avg,cell_peri_std,cell_peri_1QR,cell_peri_3QR)
+Main_Shape_Data[nrow(Main_Shape_Data) + 1, ] <- c('Dolichospermum','F199','20X','Radius',cell_radius_avg,cell_radius_std,cell_radius_1QR,cell_radius_3QR)
 
 rm(list = setdiff(ls(),c("Main_Shape_Data")))
 
